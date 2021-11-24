f55eb28e0c408af1a1dcfe65987133de0d5691f1
Andrew Reed <areed@cloudera.com>
2021-11-24 21:56:59 +0000

    working, in progress commit

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@
 
 import cdsw
 import pickle
-import sklearn
+# import sklearn
 import pandas as pd
 import numpy as np
 
@@ -36,8 +36,8 @@ def predict(data_input):
     
     # Log raw input values of features used in inference pipeline
 #     active_features = get_active_feature_names(model.named_steps["preprocess"]) feature not compatible with Python 3.6
-    active_features = ["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "waterfront", "zipcode", "condition", "view"]
-    cdsw.track_metric("input_features", df[active_features].to_dict(orient="records")[0])
+#     active_features = ["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "waterfront", "zipcode", "condition", "view"]
+#     cdsw.track_metric("input_features", df[active_features].to_dict(orient="records")[0])
 
     # Use pipeline to make inference on request
     result = model.predict(df).item()
