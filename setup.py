aeefe2aef37def42d6a1e9c31b1c525d75722c2c
Andrew Reed <areed@cloudera.com>
2021-11-24 17:14:16 +0000

    add repo setup artifacts

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="monitoring",
+    version="0.0.1",
+    description="A Cloudera Applied Machine Learning Prototype (AMP) to demonstrate continuous monitoring on CML.",
+    author="Andrew Reed"
+)
