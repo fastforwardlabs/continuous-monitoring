a455743f54130d63c625433c77414d308e8a8f01
Andrew Reed <andrew.reed.r@gmail.com>
2021-12-01 19:03:19 -0500

    fix log directory

--- src/simulation.py
+++ src/simulation.py
@@ -18,7 +18,7 @@ logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-log_file = "../logs/simulation.log"
+log_file = "logs/simulation.log"
 os.makedirs(os.path.dirname(log_file), exist_ok=True)
 file_handler = logging.FileHandler(log_file)
 file_handler.setLevel(logging.DEBUG)
