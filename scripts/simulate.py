c7a8fc3093df9fe90e3a1bbc627f1ca721513991
Andrew Reed <andrew.reed.r@gmail.com>
2021-12-02 10:45:30 -0500

    add dev_mode arg to simulate script

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -9,5 +9,7 @@ prod_path = "data/working/prod_df.pkl"
 train_df = pd.read_pickle(train_path)
 prod_df = pd.read_pickle(prod_path)
 
-sim = Simulation(model_name="Price Regressor")
+sim = Simulation(
+    model_name="Price Regressor", dev_mode=eval(os.environ["DEV_MODE"].capitalize())
+)
 sim.run_simulation(train_df, prod_df)
